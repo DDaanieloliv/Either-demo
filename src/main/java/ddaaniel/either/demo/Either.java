51190c45accd0fc2a442072ced591bb91dceee1c
ddaaniel <daniel0333v@gmail.com>
2026-01-28 16:30:21 -0300

    style: add comment strings

--- src/main/java/ddaaniel/either/demo/Either.java
+++ src/main/java/ddaaniel/either/demo/Either.java
@@ -7,18 +7,18 @@ public class Either<L, R> {
 	public R right(){ return null; } // default signature to be oberrided and accessed by the super class
 
 	public final static class Left<L, R> extends Either<L, R> { // to inherit a Generic<L, R>, is required be a Generic<L, R> && L(Left) -> Error
-		private L l;
-		public Left(L l){ this.l = l; }
-		public L left(){ return l; }
+		private L l; // instance of the Left type reference (Error)
+		public Left(L l){ this.l = l; } // Left type reference constructor's
+		public L left(){ return l; } // function that overrides the super class method 'left()'
 	}
 
 	public static class Right<L, R> extends Either<L, R> { // to inherit a Generic<L, R>, is required be a Generic<L, R>
-		private R r;
-		public Right(R r){ this.r = r; }
-		public R right() { return r; }
+		private R r; // instance of the Right type reference (Result)
+		public Right(R r){ this.r = r; } // Right type reference constructor's
+		public R right() { return r; } // function that overrides the super class method 'right()' 
 	}
 
-	public <C> C either(Map<L, C> f, Map<R, C> g) {
+	public <C> C either(Map<L, C> f, Map<R, C> g) { // <C> create a new -> reserved_space / type
 		if (this instanceof Left) return f.apply(left());
 		return g.apply(right());
 	}
