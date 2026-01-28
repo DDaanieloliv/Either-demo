d4d1784b472fbe607458ea5473aff45c25233106
ddaaniel <daniel0333v@gmail.com>
2026-01-28 12:01:31 -0300

    style: add comment strings

--- src/main/java/ddaaniel/either/demo/Either.java
+++ src/main/java/ddaaniel/either/demo/Either.java
@@ -1,29 +1,29 @@
 package ddaaniel.either.demo;
 
 
-public class Either<A, B> {
+public class Either<L, R> {
 	private Either() { } // closed hierarchy
-	public A left(){ return null; } // default signature to be oberrided and accessed by the super class
-	public B right(){ return null; } // default signature to be oberrided and accessed by the super class
+	public L left(){ return null; } // default signature to be oberrided and accessed by the super class
+	public R right(){ return null; } // default signature to be oberrided and accessed by the super class
 
-	public final static class Left<A, B> extends Either<A, B> { 
-		private A a;
-		public Left(A a){ this.a = a; }
-		public A left(){ return a; }
+	public final static class Left<L, R> extends Either<L, R> { // to inherit a Generic<L, R>, is required be a Generic<L, R> && L(Left) -> Error
+		private L l;
+		public Left(L l){ this.l = l; }
+		public L left(){ return l; }
 	}
 
-	public static class Right<A, B> extends Either<A, B> {
-		private B b;
-		public Right(B b){ this.b = b; }
-		public B right() { return b; }
+	public static class Right<L, R> extends Either<L, R> { // to inherit a Generic<L, R>, is required be a Generic<L, R>
+		private R r;
+		public Right(R r){ this.r = r; }
+		public R right() { return r; }
 	}
 
-	public <C> C either(Map<A, C> f, Map<B, C> g) {
+	public <C> C either(Map<L, C> f, Map<R, C> g) {
 		if (this instanceof Left) return f.apply(left());
 		return g.apply(right());
 	}
 }
 
-interface Map<A, B> {
-	B apply(A a);
+interface Map<L, R> {
+	R apply(L l);
 }
